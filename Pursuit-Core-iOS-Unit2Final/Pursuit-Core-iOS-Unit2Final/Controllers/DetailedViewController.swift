78f7634a7d31c9c26f29063fbfd53382fc2c4296
ChristianHurtado29 <christianhurtado@pursuit.org>
2019-11-22 12:07:08 -0500

    cells are configured correctly, working on second view controller

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailedViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailedViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Christian Hurtado on 11/22/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+    
+    
+    @IBOutlet weak var crayonNameLabel:UILabel!
+    
+    
+    @IBOutlet weak var redSlider:UISlider!
+    @IBOutlet weak var greenSlider:UISlider!
+    @IBOutlet weak var blueSlider:UISlider!
+    
+    
+    @IBOutlet weak var alphaStepper:UIStepper!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        view.backgroundColor = 
+
+    }
+    
+}
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var crayons = [Crayon](){
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    tableView.dataSource = self
+    loadData()
   }
-
-
+    func loadData() {
+        crayons = Crayon.allTheCrayons
+    }
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        crayons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        let crayon = crayons[indexPath.row]
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = crayon.hex
+        cell.backgroundColor = UIColor(red: CGFloat(crayon.red/255)
+            , green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255
+        ), alpha: 0.7)
+        return cell
+    }
+}
